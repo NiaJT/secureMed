4c5fe7ddb32e12ecf14d5f6d0f5dc275dac19dfb
UDBO <nirajthapa457@gmail.com>
2025-06-10 21:56:13 +0545

    added cors successfully

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 import connectDB from "./connectDB.js";
 import { UserController } from "./user/userController.js";
 import { PatientController } from "./patientData/patientController.js";
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://192.168.1.9:3000/"],
+  })
+);
 connectDB();
 app.use("/user", UserController);
 app.use("/patient-data", PatientController);
