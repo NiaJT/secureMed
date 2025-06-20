fd574b3ea3dac072d5a7599287c734e2b1013eb7
Niraj Thapa <nirajthapa457@gmail.com>
2025-06-20 19:55:32 +0545

    added verify page

---
+++ securemedqr/app/(auth)/verify-reports/page.tsx
@@ -0,0 +1,8 @@
+import VerifyReportPage from "@/Components/ReportComponents/VerifyReportPage";
+import React from "react";
+
+const page = () => {
+  return <VerifyReportPage />;
+};
+
+export default page;
